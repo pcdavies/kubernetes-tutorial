7a27edc811e695a3faab04024134e2b9da594bf1
pcdavies <daviespatickc@gmail.com>
2018-12-20 09:01:48 -0700

    spelling fix

--- scripts/04-trafficShiftAll.sh
+++ scripts/04-trafficShiftAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Shift All traffice to V3'
+echo 'Shift All traffic to V3'
 
 cat ~/istio-1.0.4/samples/bookinfo/networking/virtual-service-reviews-v3.yaml
 kubectl apply -f ~/istio-1.0.4/samples/bookinfo/networking/virtual-service-reviews-v3.yaml

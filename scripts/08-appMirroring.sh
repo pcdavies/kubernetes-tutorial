f8be2d3eca94fa2ea6f3863b626faa5387642e4a
pcdavies <daviespatickc@gmail.com>
2019-01-07 16:27:07 -0700

    fixed

--- scripts/08-appMirroring.sh
+++ scripts/08-appMirroring.sh
@@ -146,21 +146,26 @@ done
 
 
 export SLEEP_POD=$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})
+echo ' '
+echo 'Calling URL from pod 4 times - curl  http://httpbin:8080/headers....'
 # kubectl exec -it $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers' > /dev/null 2>&1
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers' > /dev/null 2>&1
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers' > /dev/null 2>&1
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers' > /dev/null 2>&1
 
 echo ' '
 echo 'V1 Logs without mirroring:'
+echo ' '
 export V1_POD=$(kubectl get pod -l app=httpbin,version=v1 -o jsonpath={.items..metadata.name})
 kubectl logs $V1_POD -c httpbin | grep GET
 
 echo ' '
 echo 'V2 Logs without mirroring:'
+echo ' '
 export V2_POD=$(kubectl get pod -l app=httpbin,version=v2 -o jsonpath={.items..metadata.name})
 kubectl logs $V2_POD -c httpbin | grep GET
+echo ' '
 
 cat <<EOF | kubectl apply -f -
 apiVersion: networking.istio.io/v1alpha3
@@ -181,21 +186,26 @@ spec:
       subset: v2
 EOF
 
+echo ' '
 export SLEEP_POD=$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})
 echo $SLEEP_POD
 
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers' 
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
-kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'
+echo ' '
+echo 'Calling URL from pod 4 times - curl  http://httpbin:8080/headers....'
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'  > /dev/null 2>&1
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'  > /dev/null 2>&1
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'  > /dev/null 2>&1
+kubectl exec $SLEEP_POD -c sleep -- sh -c 'curl  http://httpbin:8080/headers'  > /dev/null 2>&1
 
 echo ' '
 echo 'V1 Logs after mirroring:'
 export V1_POD=$(kubectl get pod -l app=httpbin,version=v1 -o jsonpath={.items..metadata.name})
 kubectl logs $V1_POD -c httpbin | grep GET
+echo ' '
 
 echo 'V2 Logs after mirroring:'
-
 export V2_POD=$(kubectl get pod -l app=httpbin,version=v2 -o jsonpath={.items..metadata.name})
 kubectl logs $V2_POD -c httpbin | grep GET
+echo ' '
+
 

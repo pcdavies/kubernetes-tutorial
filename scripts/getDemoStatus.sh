739a9bbe58d08ae1b371341fcf8beddb8e022ea0
pcdavies <daviespatickc@gmail.com>
2019-01-07 09:51:59 -0700

    set env variables

--- scripts/getDemoStatus.sh
+++ scripts/getDemoStatus.sh
@@ -14,6 +14,12 @@ echo ''
 echo 'Checking Pods'
 kubectl get pods -o wide
 
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o 'jsonpath={.items[0].status.hostIP}')
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo $GATEWAY_URL
+
 echo ''
 echo 'Checking for 200 return code from URL'
 curl -o /dev/null -s -w "%{http_code}\n" http://${GATEWAY_URL}/productpage

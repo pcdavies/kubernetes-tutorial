dd1d8db078ed5702c5636e672ecb565a8b71e146
pcdavies <daviespatrickc@gmail.com>
2019-01-07 22:07:49 -0500

    added unmirror

---
+++ scripts/08.6.unmirror.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: httpbin
+spec:
+  hosts:
+    - httpbin
+  http:
+  - route:
+    - destination:
+        host: httpbin
+        subset: v1
+      weight: 100
+EOF
+
+kubectl get virtualServices
+

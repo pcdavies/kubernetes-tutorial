a46d21b7721baae04d3ea32f494f06b098776482
pcdavies <daviespatrickc@gmail.com>
2019-01-07 22:02:51 -0500

    Added more logging

--- scripts/08.3.routeLogs.sh
+++ scripts/08.3.routeLogs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl describe virtualservice httpbin
+
 export SLEEP_POD=$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})
 echo ' '
 echo 'Calling URL from pod 4 times - curl  http://httpbin:8080/headers....'
@@ -15,11 +17,15 @@ export V1_POD=$(kubectl get pod -l app=httpbin,version=v1 -o jsonpath={.items..m
 
 echo '$ kubectl logs '"$V1_POD"
 kubectl logs $V1_POD -c httpbin | grep GET
+CNT=$(kubectl logs $V1_POD -c httpbin | grep GET | wc -l)
+echo 'Returned '"$CNT"' Records'
 
 echo ' '
 echo 'V2 Logs without mirroring:'
 export V2_POD=$(kubectl get pod -l app=httpbin,version=v2 -o jsonpath={.items..metadata.name})
 echo '$ kubectl logs '"$V1_POD"
 kubectl logs $V2_POD -c httpbin | grep GET
+CNT=$(kubectl logs $V2_POD -c httpbin | grep GET | wc -l)
+echo 'Returned '"$CNT"' Records'
 echo ' '
 
--- scripts/08.5.routeLogs.sh
+++ scripts/08.5.routeLogs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl describe virtualservice httpbin
+
 echo ' '
 export SLEEP_POD=$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})
 echo $SLEEP_POD
@@ -16,12 +18,16 @@ echo 'V1 Logs after mirroring:'
 export V1_POD=$(kubectl get pod -l app=httpbin,version=v1 -o jsonpath={.items..metadata.name})
 echo '$ kubectl logs '"$V1_POD"
 kubectl logs $V1_POD -c httpbin | grep GET
+CNT=$(kubectl logs $V1_POD -c httpbin | grep GET | wc -l)
+echo 'Returned '"$CNT"' Records'
 echo ' '
 
 echo 'V2 Logs after mirroring:'
 export V2_POD=$(kubectl get pod -l app=httpbin,version=v2 -o jsonpath={.items..metadata.name})
 echo '$ kubectl logs '"$V2_POD"
 kubectl logs $V2_POD -c httpbin | grep GET
+CNT=$(kubectl logs $V2_POD -c httpbin | grep GET | wc -l)
+echo 'Returned '"$CNT"' Records'
 echo ' '
 
 

a500dd6b3a1394a6cbdae51f68429f09b58850a3
pcdavies <daviespatrickc@gmail.com>
2019-01-28 09:54:16 -0700

    adding reset

---
+++ samples/istio/08-resetToV1NoFailure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$ISTIO_DIR" = "" ]; then
+    echo 'ISTIO_DIR environment is not set. Example: /home/kubeuser/istio-1.0.5'
+    exit
+fi
+
+echo 'Routing all request to Version 1'
+
+kubectl apply -f $ISTIO_DIR/samples/bookinfo/networking/virtual-service-all-v1.yaml
+
+
+

38a7d158f5f885859bfd14df7f17afd22239e9cf
pcdavies <daviespatickc@gmail.com>
2019-02-04 12:05:11 -0700

    Updates

--- samples/istio/09.8-addRemoteMySQLDB.sh
+++ samples/istio/09.8-addRemoteMySQLDB.sh
@@ -66,9 +66,9 @@ spec:
           - name: DB_TYPE
             value: "mysql"
           - name: MYSQL_DB_HOST
-            value: \"'$MYSQL_HOST'\"
+            value: "192.168.168.193"
           - name: MYSQL_DB_PORT
-            value: \"'$MYSQL_PORT'\"
+            value: "3306"
           - name: MYSQL_DB_USER
             value: $MYSQL_USER
           - name: MYSQL_DB_PASSWORD
